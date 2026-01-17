c091e2f2ab1b3035eb7fabe340257c3d56710348
nkot20 <74417927+nkot20@users.noreply.github.com>
2026-01-17 16:56:05 +0100

    remove unit tests

--- src/test/java/com/sendByOP/expedition/ExpeditionApplicationTests.java
+++ src/test/java/com/sendByOP/expedition/ExpeditionApplicationTests.java
@@ -3,11 +3,8 @@ package com.sendByOP.expedition;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ExpeditionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
